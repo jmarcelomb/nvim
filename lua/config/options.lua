b46f46d7776e91b5de83e12bda29a59cd9af4b91
Marcelo Borges <jmmb1098@gmail.com>
2025-01-05 13:21:07 +0000

    removes neovide animations, adds surround

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,10 +25,11 @@ vim.b.autoformat = false
 
 vim.o.clipboard = "unnamedplus"
 
--- vim.g.neovide_position_animation_length = 0
--- vim.g.neovide_cursor_animation_length = 0.00
--- vim.g.neovide_cursor_trail_size = 0
--- vim.g.neovide_cursor_animate_in_insert_mode = false
--- vim.g.neovide_cursor_animate_command_line = false
--- vim.g.neovide_scroll_animation_far_lines = 0
--- vim.g.neovide_scroll_animation_length = 0.00
+-- Disable neovide animations
+vim.g.neovide_position_animation_length = 0
+vim.g.neovide_cursor_animation_length = 0.00
+vim.g.neovide_cursor_trail_size = 0
+vim.g.neovide_cursor_animate_in_insert_mode = false
+vim.g.neovide_cursor_animate_command_line = false
+vim.g.neovide_scroll_animation_far_lines = 0
+vim.g.neovide_scroll_animation_length = 0.00
