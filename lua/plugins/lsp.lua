dd77684059da7eb1aa74c64ca987af28e5b9f7f0
Marcelo Borges <me@marceloborges.dev>
2025-01-19 16:41:57 +0000

    adds rust-analyzer and bacon-ls in mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,31 +1,43 @@
 return {
-  "neovim/nvim-lspconfig",
-  ---@class PluginLspOpts
-  opts = {
-    ---@type lspconfig.options
-    servers = {
-      -- pyright will be automatically installed with mason and loaded with lspconfig
-      pyright = {
-        settings = {
-          typos_lsp = {},
-          pyright = {
-            -- Using Ruff's import organizer
-            disableOrganizeImports = true,
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {
+          settings = {
+            typos_lsp = {},
+            pyright = {
+              -- Using Ruff's import organizer
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                -- Ignore all files for analysis to exclusively use Ruff for linting
+                ignore = { "*" },
+              },
+            },
           },
-          python = {
-            analysis = {
-              -- Ignore all files for analysis to exclusively use Ruff for linting
-              ignore = { "*" },
+        },
+        ruff = {
+          trace = "messages",
+          init_options = {
+            settings = {
+              logLevel = "debug",
             },
           },
         },
       },
-      ruff = {
-        trace = "messages",
-        init_options = {
-          settings = {
-            logLevel = "debug",
-          },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          mason = false,
         },
       },
     },
