0a47c4008074d1aafce94dca3d1053b8d1b2a4a8
Marcelo Borges <jmmb1098@gmail.com>
2025-02-10 09:33:22 +0000

    refactor: reorder lsp plugin configuration in lsp.lua
    The lsp_lines.nvim plugin is moved to the end of the configuration list and
    now includes `event` and `priority` options. This ensures the plugin is
    loaded at the correct time with higher priority, preventing potential
    conflicts or initialization issues with other LSP-related plugins.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,24 +1,5 @@
 return {
   {
-    "maan2003/lsp_lines.nvim",
-    config = function()
-      -- Disable virtual text explicitly before setting up lsp_lines
-      vim.diagnostic.config({ virtual_text = false })
-      require("lsp_lines").setup()
-    end,
-    keys = {
-      {
-        "<leader>lt",
-        function()
-          local lsp_lines = require("lsp_lines")
-          local state = lsp_lines.toggle()
-          vim.diagnostic.config({ virtual_text = not state }) -- Toggle virtual text when disabling lsp_lines
-        end,
-        desc = "Toggle lsp_lines",
-      },
-    },
-  },
-  {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
@@ -58,6 +39,27 @@ return {
         clangd = {
           mason = false,
         },
+      },
+    },
+  },
+  {
+    "maan2003/lsp_lines.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      -- Disable virtual text explicitly before setting up lsp_lines
+      vim.diagnostic.config({ virtual_text = false })
+      require("lsp_lines").setup()
+    end,
+    keys = {
+      {
+        "<leader>lt",
+        function()
+          local lsp_lines = require("lsp_lines")
+          local state = lsp_lines.toggle()
+          vim.diagnostic.config({ virtual_text = not state }) -- Toggle virtual text when disabling lsp_lines
+        end,
+        desc = "Toggle lsp_lines",
       },
     },
   },
