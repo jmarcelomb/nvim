9fa15ff7132fee0c13c118c6fc73f89c4faa720d
Marcelo Borges <marcelo.borges@synopsys.com>
2025-11-01 12:45:54 +0000

    fix(dap): restructure plugin config and enable dap-ui
    - Fix table structure for nvim-dap plugin spec
    - Enable dap-helper.nvim plugin for debugging assistance
    - Add nvim-dap-ui with automatic open/close listeners
    - Configure keymaps for UI toggle and eval functions
    - Disable nvim-dap-python plugin

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,6 +1,7 @@
 return {
   {
-    "mfussenegger/nvim-dap",
+    {
+      "mfussenegger/nvim-dap",
     -- stylua: ignore
     keys = {
       { "<leader>d", "", desc = "+debug", mode = {"n", "v"} },
@@ -24,10 +25,36 @@ return {
     { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
     { "<leader>de", function() require("dapui").eval(nill, { enter = true }) end, desc = "Eval", mode = {"n", "v"} },
     },
+    },
   },
-  -- {
-  --   "daic0r/dap-helper.nvim",
-  --   event = "VeryLazy",
-  --   opts = {},
-  -- },
+  {
+    "daic0r/dap-helper.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "nvim-neotest/nvim-nio" },
+  -- stylua: ignore
+  keys = {
+    { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+  },
+    opts = {},
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        -- dapui.close({})
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        -- dapui.close({})
+      end
+    end,
+  },
+  -- { "mfussenegger/nvim-dap-python", enabled = false },
 }
